d3bc45933b1536974e759bed050b7259fdcb6806
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 13:47:43 +0530

    test

--- p/lambda.js
+++ p/lambda.js
@@ -1,18 +1,3 @@
-let SL_AWS = require('slappforge-sdk-aws');
-let connectionManager = require('./ConnectionManager');
-const rds = new SL_AWS.RDS(connectionManager);
-
 exports.handler = function (event, context, callback) {
-
-    // You must always end/destroy the DB connection after it's used
-    rds.beginTransaction({
-        instanceIdentifier: 's'
-    }, function (error, connection) {
-        if (error) {
-            throw error;
-        }
-        connection.end();
-    });
-
     callback(null, { "message": "Successfully executed" });
 }
