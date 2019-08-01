bcc26206bf6bba80ae01c47fa756efc75a243eb9
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 13:16:23 +0530

    test

--- p/lambda.js
+++ p/lambda.js
@@ -4,22 +4,13 @@ const rds = new SL_AWS.RDS(connectionManager);
 
 exports.handler = function (event, context, callback) {
 
-    // You can pass the existing connection to this function.
-    // A new connection will be created if it's not present as the third param 
     // You must always end/destroy the DB connection after it's used
-    rds.query({
-        instanceIdentifier: 'l',
-        query: 'INSERT INTO Test (ID);',
-        inserts: [1]
-    }, function (error, results, connection) {
+    rds.beginTransaction({
+        instanceIdentifier: 'k'
+    }, function (error, connection) {
         if (error) {
-            console.log("Error occurred");
             throw error;
-        } else {
-            console.log("Success")
-            console.log(results);
         }
-
         connection.end();
     });
 
