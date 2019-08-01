92ed29831b060708e5cbdfd8760c36b37c8a4141
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 11:16:27 +0530

    test

--- p/ConnectionManager.js
+++ p/ConnectionManager.js
@@ -1,11 +1,11 @@
 module.exports = function() {
     this.dbConnections = [];
 
-    this.dbConnections["K"] = {
+    this.dbConnections["k"] = {
         host: process.env.EndPoint_rdsK,
         port: process.env.Port_rdsK,
         user: process.env.User_rdsK,
         password: process.env.Password_rdsK,
-        database: "Kk"
+        database: "k"
     };
 };
--- p/lambda.js
+++ p/lambda.js
@@ -4,13 +4,22 @@ const rds = new SL_AWS.RDS(connectionManager);
 
 exports.handler = function (event, context, callback) {
 
+    // You can pass the existing connection to this function.
+    // A new connection will be created if it's not present as the third param 
     // You must always end/destroy the DB connection after it's used
-    rds.beginTransaction({
-        instanceIdentifier: 'K'
-    }, function (error, connection) {
+    rds.query({
+        instanceIdentifier: 'k',
+        query: 'INSERT INTO Test(ID);',
+        inserts: [1]
+    }, function (error, results, connection) {
         if (error) {
+            console.log("Error occurred");
             throw error;
+        } else {
+            console.log("Success")
+            console.log(results);
         }
+
         connection.end();
     });
 
